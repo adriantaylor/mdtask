d250689a46143b15fc36c4919146679d38eeb0c8
Ubuntu <at@GoDev.cau3k033a3oubllylrcmnjys3d.fx.internal.cloudapp.net>
2019-10-07 18:36:24 +0000

    round 1

---
+++ mdtask.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"github.com/gomarkdown/markdown"
+//	"github.com/gomarkdown/markdown/parser"
+)
+
+type Article struct {
+	Filename string
+	Body []byte
+}
+
+func loadFile(title string) *Article {
+	filename := title + ".md"
+	body, _ := ioutil.ReadFile(filename)
+	return &Article{Filename: filename, Body: body}
+}
+
+func main() {
+	File := loadFile("test")
+	md := File.Body
+	html := markdown.ToHTML(md, nil, nil)
+	fmt.Printf("%s\n", html)
+}
+
